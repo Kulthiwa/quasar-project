0d9acb240b09449d9c3ee0611decbe854f974a5f
Kulthiwa <pngtchanel@gmail.com>
2025-07-19 22:47:17 +0700

    แก้ครั้งท้ายละ

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,52 +1,78 @@
 <template>
-  <q-page class="q-pa-md">
-
-    <h5>กรอกข้อมูล</h5>
-
-    <q-form @submit="onSubmit">
-
+  <div class="q-pa-md" style="max-width: 400px">
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
       <q-input
+        filled
         v-model="name"
-        label="ชื่อ"
-        outlined
-        dense
-        required
+        label="ชื่อ-สกุล: *"
+        hint="กรุณาใส่ชื่อและนามสกุล"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'กรุณาใส่ข้อความที่ถูกต้อง']"
       />
 
       <q-input
-        v-model.number="age"
-        label="อายุ"
+        filled
         type="number"
-        outlined
-        dense
-        required
-        class="q-mt-md"
+        v-model="age"
+        label="อายุ: *"
+        lazy-rules
+        :rules="[
+          (val) => (val !== null && val !== '') || 'กรุณาพิมพ์อายุของคุณนะครับ',
+          (val) => (val > 0 && val < 100) || 'กรุณาใส่อายุที่ถูกต้องนะจ๊ะ',
+        ]"
       />
 
-      <q-btn
-        label="บันทึก"
-        type="submit"
-        color="primary"
-        class="q-mt-lg"
-      />
+      <q-toggle v-model="accept" label="I accept the license and terms" />
+
+      <div>
+        <q-btn label="ยืนยัน" type="submit" color="primary" />
+        <q-btn label="เริ่มใหม่" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
     </q-form>
-
-  </q-page>
+  </div>
 </template>
 
-<script setup>
+<script>
+import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 
-const name = ref('')
-const age = ref(null)
+export default {
+  setup() {
+    const $q = useQuasar()
 
-function onSubmit () {
-  console.log('ชื่อ:', name.value)
-  console.log('อายุ:', age.value)
-  alert(`ชื่อ: ${name.value}, อายุ: ${age.value}`)
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
 
+    return {
+      name,
+      age,
+      accept,
+
+      onSubmit() {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first',
+          })
+        } else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted',
+          })
+        }
+      },
+
+      onReset() {
+        name.value = null
+        age.value = null
+        accept.value = false
+      },
+    }
+  },
 }
 </script>
-
-<style scoped>
-</style>
