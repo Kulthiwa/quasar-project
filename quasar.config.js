80249619ceb5e20f84e0ae5e85e38853d62ee165
Kulthiwa <pngtchanel@gmail.com>
2025-10-08 11:23:06 +0700

    Commit 5: CI/CD

--- quasar.config.js
+++ quasar.config.js
@@ -41,6 +41,7 @@ export default defineConfig((/* ctx */) => {
       },
 
       vueRouterMode:  'history', // available values: 'hash', 'history'
+      publicPath: '/quasar-project/',
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
